0086ade3df514fea0c4997dd277c306074abd626
Alexey Makarov <al3x.mak.89@gmail.com>
2017-01-30 17:38:49 +0300

    update objectLength.js

---
+++ objectLength.js
@@ -0,0 +1,7 @@
+var countInternals = function(obj) {
+  var count = 0;
+  for (var key in obj) {
+  	count++;
+	}
+	return count;
+}
--- objectlength.js
+++
@@ -1,7 +0,0 @@
-var countInternals = function(obj) {
-    var count = 0;
-    for (var key in obj) {
-    	count++;
-	}
-	return count;
-}
