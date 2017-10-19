599db2f05308a5f6ed44f6b3be938f5134223fee
Alexey Makarov <al3x.mak.89@gmail.com>
2017-10-19 16:19:13 +0300

    Create addExternalLibInConsole.js

---
+++ addExternalLibInConsole.js
@@ -0,0 +1,3 @@
+var script = document.createElement('script');
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
